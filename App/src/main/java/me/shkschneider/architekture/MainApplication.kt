58e120a70b62621acd6df62266758ff70e1cf22f
shkschneider <shk.schneider@gmail.com>
2019-05-18 21:48:56 +0200

    Cleanup

--- App/src/main/java/me/shkschneider/architekture/MainApplication.kt
+++ App/src/main/java/me/shkschneider/architekture/MainApplication.kt
@@ -1,13 +1,21 @@
 package me.shkschneider.architekture
 
 import android.app.Application
+import me.shkschneider.order.OrderModule
+import me.shkschneider.product.ProductModule
+import me.shkschneider.user.UserModule
+import org.koin.android.ext.android.startKoin
 
 @Suppress("unused")
 class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        dependencyInjection()
+        startKoin(applicationContext, listOf(
+            OrderModule.module,
+            ProductModule.module,
+            UserModule.module
+        ))
     }
 
 }
