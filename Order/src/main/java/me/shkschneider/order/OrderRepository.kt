07e8ac89a6eca1a8b514b180712fef1a13bac354
shkschneider <shk.schneider@gmail.com>
2019-05-18 20:19:59 +0200

    Cleanup

--- Order/src/main/java/me/shkschneider/order/OrderRepository.kt
+++ Order/src/main/java/me/shkschneider/order/OrderRepository.kt
@@ -1,12 +1,10 @@
 package me.shkschneider.order
 
-import me.shkschneider.order.Order
 import me.shkschneider.product.Product
 import me.shkschneider.user.User
 
 class OrderRepository {
 
-    fun getOrder(user: me.shkschneider.user.User, product: me.shkschneider.product.Product) =
-        me.shkschneider.order.Order(user = user, product = product)
+    fun getOrder(user: User, product: Product) = Order(user = user, product = product)
 
 }
